bbdb65c3488cf06d3e0e77aee71f3ff9e558c352
vagrant <vagrant@precise32.(none)>
2014-07-19 03:56:30 +0000

    Lesson #13

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,11 @@
 module.exports.helloWorld = undefined;
 
+var helloWorld = function() {
+  return 'Hello World';
+}
+
 module.exports.multiply = undefined;
+
+var multiply = function(x, y) {
+  return x*y;
+}
--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,23 @@
 module.exports.favoriteNumber = undefined;
 
+favoriteNumber = function(fav, guess) {
+  if (guess > fav) {
+    return "Too high";
+  } 
+  else if (guess < fav) {
+    return "Too low";
+  }
+  else (guess == fav) {
+    return "You got it!";
+  }
+};
+
+//Take two arguments, the first is the favorite number and the second is a guess at what the favorite number is
+//This function should return:
+//"Too high" if guess is greater than fav
+//"Too low" if guess is less than fav
+//"You got it!" if guess is equal to fav
+
 module.exports.checkLock = undefined;
 
 module.exports.canIGet = undefined;
