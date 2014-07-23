f80b40c16dfd2b74b9c73b54f237e2decd2a1881
vagrant <vagrant@precise32.(none)>
2014-07-23 12:48:34 +0000

    Lesson #18

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,13 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+  while (conditionalFn()) {actionFn()}
+};
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function(x) {
+  var one = 0;
+  var two = 0;
+  while (two < x.length) {
+  	one = x[two] + one;
+  	two++;
+  }
+  return one;
+};
