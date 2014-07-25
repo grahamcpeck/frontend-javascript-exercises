4af8c009de61bd9e225d7ef60a2707535840841b
vagrant <vagrant@precise32.(none)>
2014-07-25 12:33:24 +0000

    Lesson #18

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -3,38 +3,21 @@ module.exports.createCourse = function(courseTitle, courseDuration, courseStuden
   return course;
 };
 
-module.exports.addProperty = function(){
-
+module.exports.addProperty = function(object, newProp, newValue){
+  object[newProp] = newValue;
+  return object
 };
 
-//Complete the addProperty function. This function should:
-//Take three arguments:
-//object: an object to add a properties to
-//newProp: a property that we want to add to the object
-//newValue: a value that we want the new property to have
-//If object doesn't already have a property named newProp, then add newProp with value of newValue to object
-//If object already has newProp, return the object argument.
-
-module.exports.formLetter = function(){
-
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender
 };
 
-//Complete the formLetter function. This function should:
-//take one argument, a letter, which has three properties recipient, sender, and msg
-//combine the three properties into a single string with an additional greeting and closing
-//insert additional new lines between the greeting, message, and signature.
-
-module.exports.canIGet = function (){
-
+module.exports.canIGet = function(item, money){
+  var products = {
+  'MacBook Air': 999,
+  'MacBook Pro': 1299,
+  'Mac Pro': 2499,
+  'Apple Sticker': 1
+  };
+  return products[item] <= money;
 };
-
-//Take two arguments:
-//item: represents what the user wants to buy
-//money: represents how many dollars a user has
-//return true if a user can afford a given item according to the price chart below, and false otherwise:
-//'MacBook Air' - $999
-//MacBook Pro' - $1299
-//'Mac Pro' - $2499
-//'Apple Sticker' - $1
-//Return false if the item is not in the above list of Apple products
-//Do this with 0 'if' conditions! (Hint: Place the above price table in an object).
